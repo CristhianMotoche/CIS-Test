b413a498985020936705d6fac4ea12bbad2d1c83
CristhianMotoche <cristhian.motoche@gmail.com>
2016-08-08 19:08:19 -0500

    Update CIS files

--- cis-audit.sh
+++ cis-audit.sh
@@ -3,7 +3,18 @@
 # Este script está basado en el benchmark CIS CentOS Linux 7 v2.1.0
 # Enlace: https://benchmarks.cisecurity.org/tools2/linux/CIS_CentOS_Linux_7_Benchmark_v2.1.0.pdf
 
-# Funcion para comprobar que un sistema de archivos este deshabilitado
+# Funcion para comprobar si una particion existe
+# y si tiene cierto permiso
+function check_permision_on_partition {
+  mount | grep "${2}" | grep "${1}"
+}
+
+# Funcion para comprobar si una particion existe
+function check_partition {
+  mount | grep "${1}"
+}
+
+# Funcion para comprobar que un sistema de archivos esta deshabilitado
 function filesystem_is_disabled {
   local filesystem="${1}"
   local E_FILE_SYSTEM_DIABLED=0
@@ -43,6 +54,60 @@ function main {
 
   # CIS 1.1.1.3
   function_wrapper filesystem_is_disabled jffs2
+
+  # CIS 1.1.1.4
+  function_wrapper filesystem_is_disabled hfs
+
+  # CIS 1.1.1.5
+  function_wrapper filesystem_is_disabled hfsplus
+
+  # CIS 1.1.1.6
+  function_wrapper filesystem_is_disabled squashfs
+
+  # CIS 1.1.1.7
+  function_wrapper filesystem_is_disabled udf
+
+  # CIS 1.1.1.8 # May Remove
+  function_wrapper filesystem_is_disabled vfat
+
+  # CIS 1.1.2
+  function_wrapper check_partition /tmp
+
+  # CIS 1.1.3
+  function_wrapper check_permision_on_partition nodev /tmp
+
+  # CIS 1.1.4
+  function_wrapper check_permision_on_partition nosuid /tmp
+
+  # CIS 1.1.5
+  function_wrapper check_permision_on_partition noexec /tmp
+
+  # CIS 1.1.6
+  function_wrapper check_partition /var
+
+  # CIS 1.1.7
+  function_wrapper check_partition /var/tmp
+
+  # CIS 1.1.8
+  function_wrapper check_permision_on_partition nodev /var/tmp
+
+  # CIS 1.1.9
+  function_wrapper check_permision_on_partition nosuid /var/tmp
+
+  # CIS 1.1.10
+  function_wrapper check_permision_on_partition noexec /var/tmp
+
+  # CIS 1.1.11
+  function_wrapper check_partition /var/log
+
+  # CIS 1.1.12
+  function_wrapper check_partition /var/log/audit
+
+  # CIS 1.1.13
+  function_wrapper check_partition /home
+
+  # CIS 1.1.14
+  function_wrapper check_permision_on_partition nodev /home
 }
 
 # Ejecucion de la funcion main
--- cis-remediation.sh
+++ cis-remediation.sh
@@ -3,7 +3,16 @@
 # Este script está basado en el benchmark CIS CentOS Linux 7 v2.1.0
 # Enlace: https://benchmarks.cisecurity.org/tools2/linux/CIS_CentOS_Linux_7_Benchmark_v2.1.0.pdf
 
-# Funcion para comprobar que un sistema de archivos este deshabilitado
+# Funcion para emitir un mensaje al usuario sobre la creacion de una particion
+function disable_filesystem {
+  echo "Deberia crear la siguiente particion: ${1}"
+  echo "Puede utiliza LVM para hacerlo"
+  echo "HOWTO: http://tldp.org/HOWTO/LVM-HOWTO/"
+  echo "Y asignarle los siguientes permisos: ${2}"
+}
+
+# Funcion para añadir un sistema de archivos al archivo /etc/modprobe.d/CIS.conf
+# para deshabilitarlos
 function disable_filesystem {
   local filesystem="${1}"
   local output_file="/etc/modprobe.d/CIS.conf"
@@ -12,7 +21,7 @@ function disable_filesystem {
 
 # Funcion que envuelve a otras funciones para
 function function_wrapper {
-# comprobar si su resultado es correcto o no.
+  # comprobar si su resultado es correcto o no.
   func_name=$1
   shift
   args=$@
@@ -28,11 +37,46 @@ function function_wrapper {
 # que realizan las recomendaciones del benchmark
 function main {
   # CIS 1.1.1.1
- function_wrapper disable_filesystem cramfs
+  function_wrapper disable_filesystem cramfs
+
   # CIS 1.1.1.2
- function_wrapper disable_filesystem freevxfs
+  function_wrapper disable_filesystem freevxfs
+
   # CIS 1.1.1.3
- function_wrapper disable_filesystem jffs2
+  function_wrapper disable_filesystem jffs2
+
+  # CIS 1.1.1.4
+  function_wrapper disable_filesystem hfs
+
+  # CIS 1.1.1.5
+  function_wrapper disable_filesystem hfsplus
+
+  # CIS 1.1.1.6
+  function_wrapper disable_filesystem squashfs
+
+  # CIS 1.1.1.7
+  function_wrapper disable_filesystem udf
+
+  # CIS 1.1.1.8
+  function_wrapper disable_filesystem vfat
+
+  # CIS 1.1.[2-5]
+  function_wrapper suggest_partition /tmp "(noexec, nodev, nosuid)"
+
+  # CIS 1.1.6
+  function_wrapper suggest_partition /var ""
+
+  # CIS 1.1.{7-10}
+  function_wrapper suggest_partition /var/tmp "(noexec, nodev, nosuid)"
+
+  # CIS 1.1.11
+  function_wrapper suggest_partition /var/log ""
+
+  # CIS 1.1.12
+  function_wrapper suggest_partition /var/log/audit ""
+
+  # CIS 1.1.[13-14]
+  function_wrapper suggest_partition /home "(nodev)"
 }
 
 main
