a511382bb2f1d1172cd5da39f6e9645d8727d7ad
CristhianMotoche <cristhian.motoche@gmail.com>
2016-08-09 20:30:07 -0500

    Update scripts

--- cis-audit.sh
+++ cis-audit.sh
@@ -3,6 +3,84 @@
 # Este script está basado en el benchmark CIS CentOS Linux 7 v2.1.0
 # Enlace: https://benchmarks.cisecurity.org/tools2/linux/CIS_CentOS_Linux_7_Benchmark_v2.1.0.pdf
 
+function ensure_packet_redirect_is_disable {
+  if [[ "$(sysctl net.ipv4.conf.all.send_redirects)" == "net.ipv4.conf.all.send_redirects = 0" ]] &&\
+    [[ "$(sysctl net.ipv4.conf.default.send_redirects)" == "net.ipv4.conf.default.send_redirects = 0" ]]; then
+    return 0
+  fi
+  return 1
+}
+
+function ensure_ip_forwarding_disable {
+  if [[ "$(sysctl net.ipv4.ip_forward)" == "net.ipv4.ip_forward = 0" ]]; then
+    return 0
+  fi
+  return 1
+}
+
+function ensure_port_not_listening {
+  netstat -an | grep LIST | grep ":25[[:space:]]"
+}
+
+function ensure_service_is_disabled {
+  if [[ "$(systemctl is-enabled "${1}")" == "disabled" ]]; then
+    return 0
+  fi
+  return 1
+}
+
+function ensure_ntp_restrict {
+  grep "^restrict" /etc/ntp.conf | \
+    grep "restrict -4 default kod nomodify notrap nopeer noquery" &&\
+    grep "^server" /etc/ntp.conf
+}
+
+function ensure_ntp_installed {
+  rpm -q ntp | grep "ntp*"
+  return $?
+}
+
+function ensure_filesystem_integrity {
+  crontab -u root -l | grep aide
+  grep -r aide /etc/cron.* /etc/crontab
+}
+
+function ensure_is_installed {
+  rpm -q "${1}" | grep ${1}"*"
+  return $?
+}
+
+function check_gpg_global_is_active {
+  grep ^gpgcheck /etc/yum.conf
+}
+
+function ensure_gpg_keys {
+  rpm -q gpg-pubkey --qf '%{name}-%{version}-%{release} --> %{summary}\n'
+}
+
+function check_repositories {
+  yum repolist
+}
+
+function check_autofs_disabled {
+  local IS_AUTOFS_ENABLED=0
+  local result="$(systemctl is-enabled autofs)"
+
+  if [[ "${result}" == "disabled" ]]; then
+    return "${IS_AUTOFS_ENABLED}"
+  else
+    return 1
+  fi
+}
+
+function check_permision_on_external_partition {
+  mount | grep "${2}" | grep "${1}"
+}
+
+function check_sticky_bit {
+  df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null
+}
+
 # Funcion para comprobar si una particion existe
 # y si tiene cierto permiso
 function check_permision_on_partition {
@@ -46,6 +124,7 @@ function function_wrapper {
 # Funcion principal que realiza la llamada a las funciones
 # que comprueban las auditorias del benchmark
 function main {
+  # FILESYSTEM
   # CIS 1.1.1.1
   function_wrapper filesystem_is_disabled cramfs
 
@@ -108,7 +187,116 @@ function main {
 
   # CIS 1.1.14
   function_wrapper check_permision_on_partition nodev /home
+
+  # CIS 1.1.15
+  function_wrapper check_permision_on_partition nodev /dev/shm
+
+  # CIS 1.1.16
+  function_wrapper check_permision_on_partition nosuid /dev/shm
+
+  # CIS 1.1.17
+  function_wrapper check_permision_on_partition noexec /dev/shm
+
+  # CIS 1.1.[18-20]
+  function_wrapper check_permision_on_external_partition nodev
+  function_wrapper check_permision_on_external_partition nosuid
+  function_wrapper check_permision_on_external_partition noexec
+
+  # CIS 1.1.21
+  function_wrapper check_sticky_bit
+
+  # CIS 1.1.22 # May Remove
+  function_wrapper check_autofs_disabled
+
+  # CIS 1.2.1
+  function_wrapper check_repositories
+
+  # CIS 1.2.2
+  function_wrapper ensure_gpg_keys
+
+  # CIS 1.2.3
+  function_wrapper check_gpg_global_is_active
+
+  # CIS 1.3.1
+  function_wrapper ensure_is_installed aide
+
+  # CIS 1.3.2
+  function_wrapper ensure_filesystem_integrity
+
+  # SERVICES
+  # CIS 2.2.1.1
+  function_wrapper ensure_is_installed ntp
+
+  # CIS 2.2.1.2
+  function_wrapper ensure_ntp_restrict
+
+  # CIS 2.2.3
+  function_wrapper ensure_service_is_disabled avahi-daemon
+
+  # CIS 2.2.4
+  function_wrapper ensure_service_is_disabled cups
+
+  # CIS 2.2.5
+  function_wrapper ensure_service_is_disabled dhcpd
+
+  # CIS 2.2.6
+  function_wrapper ensure_service_is_disabled slapd
+
+  # CIS 2.2.7
+  function_wrapper ensure_service_is_disabled nfs
+
+  # CIS 2.2.8
+  function_wrapper ensure_service_is_disabled named
+
+  # CIS 2.2.9
+  function_wrapper ensure_service_is_disabled vsftpd
+
+  # CIS 2.2.10
+  function_wrapper ensure_service_is_disabled httpd
+
+  # CIS 2.2.11
+  function_wrapper ensure_service_is_disabled dovecot
+
+  # CIS 2.2.12
+  function_wrapper ensure_service_is_disabled smb
+
+  # CIS 2.2.13
+  function_wrapper ensure_service_is_disabled squid
+
+  # CIS 2.2.14
+  function_wrapper ensure_service_is_disabled snmpd
+
+  # CIS 2.2.16
+  function_wrapper ensure_service_is_disabled ypserv
+
+  # CIS 2.2.17
+  function_wrapper ensure_service_is_disabled rsh.socket
+  function_wrapper ensure_service_is_disabled rlogin.socket
+  function_wrapper ensure_service_is_disabled rexec.socket
+
+  # CIS 2.2.18
+  function_wrapper ensure_service_is_disabled telnet.socket
+
+  # CIS 2.2.19
+  function_wrapper ensure_service_is_disabled tftp.socket
+
+  # CIS 2.2.20
+  function_wrapper ensure_service_is_disabled rsyncd
+
+  # CIS 2.2.21
+  function_wrapper ensure_service_is_disabled ntalk
+
+  # NETWORK CONFIGURATION
+  # CIS 3.1.1
+  function_wrapper ensure_ip_forwarding_disable
+
+  # CIS 3.1.2
+  function_wrapper ensure_packet_redirect_is_disable
+
+  # CIS 3.6.1
+  function_wrapper ensure_is_installed iptables
 }
 
 # Ejecucion de la funcion main
 main
+
--- cis-remediation.sh
+++ cis-remediation.sh
@@ -3,6 +3,14 @@
 # Este script está basado en el benchmark CIS CentOS Linux 7 v2.1.0
 # Enlace: https://benchmarks.cisecurity.org/tools2/linux/CIS_CentOS_Linux_7_Benchmark_v2.1.0.pdf
 
+function disable_autfs {
+  systemctl disable autofs
+}
+
+function set_sticky_bit {
+  df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
+}
+
 # Funcion para emitir un mensaje al usuario sobre la creacion de una particion
 function disable_filesystem {
   echo "Deberia crear la siguiente particion: ${1}"
@@ -66,7 +74,7 @@ function main {
   # CIS 1.1.6
   function_wrapper suggest_partition /var ""
 
-  # CIS 1.1.{7-10}
+  # CIS 1.1.[7-10]
   function_wrapper suggest_partition /var/tmp "(noexec, nodev, nosuid)"
 
   # CIS 1.1.11
@@ -77,6 +85,18 @@ function main {
 
   # CIS 1.1.[13-14]
   function_wrapper suggest_partition /home "(nodev)"
+
+  # CIS 1.1.[15-17]
+  function_wrapper suggest_partition /dev/shm "(noexec, nodev, nosuid)"
+
+  # CIS 1.1.[18-20]
+  function_wrapper suggest_partition "EXTERNAL PARTITIONS" "(noexec, nodev, nosuid)"
+
+  # CIS 1.1.21
+  function_wrapper set_sticky_bit
+
+  # CIS 1.1.22
+  function_wrapper disable_autfs
 }
 
 main
